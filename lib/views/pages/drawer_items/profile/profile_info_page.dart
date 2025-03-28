97ac9994a197574f085522c3dbc7157de6f761b1
kspatri <136003954+kspatri@users.noreply.github.com>
2025-03-28 19:14:14 +0000

    profil info

--- lib/views/pages/drawer_items/profile/profile_info_page.dart
+++ lib/views/pages/drawer_items/profile/profile_info_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:voyagedifiant/core/constants/app_colors.dart';
+import 'package:voyagedifiant/core/constants/app_defaults.dart';
+import 'package:voyagedifiant/core/widgets/buttons/app_button.dart';
+import 'package:voyagedifiant/core/widgets/textfield/password_field.dart';
+import 'package:voyagedifiant/core/widgets/textfield/phone_input_field.dart';
 
 class ProfileInfoPage extends StatefulWidget {
   const ProfileInfoPage({super.key});
@@ -38,6 +42,52 @@ class _ProfileInfoPageState extends State<ProfileInfoPage> {
             ),
           ),
         ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(AppDefaults.padding),
+            child: Column(
+              children: [
+                SizedBox(height: 30,),
+               TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person,size:30, color: AppColors.signUpColor,),
+                  hintText: "Yabo Kao Chara Adewalé",
+                 enabledBorder: UnderlineInputBorder(
+                 borderSide: BorderSide(color: AppColors.gray, width: 2),
+                 ) ,
+                 focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.gray, width: 2)),
+                ),
+               ),
+               SizedBox(height: 25,),
+               PhoneInputField(
+               onChanged: (number) {
+                  print('Numéro : $number');
+                },
+                 onCountryChanged: (country) {
+                  print('Pays : $country');
+                },),
+                SizedBox(height: 25,),
+                TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email,size:30, color: AppColors.signUpColor,),
+                  hintText: "yaooya@gmail.com",
+                 enabledBorder: UnderlineInputBorder(
+                 borderSide: BorderSide(color: AppColors.gray, width: 2),
+                 ) ,
+                 focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.gray, width: 2)),
+                ),
+               ),
+               SizedBox(height: 30,),
+                AppCustomButton(onPressed:(){} , buttonText: "Enregistrer", 
+                   textColor: AppColors.white,
+                  buttonColor: AppColors.primaryColor,
+                 borderRadius: BorderRadius.circular(5),
+                 ),
+              ],
+            ),
+          )),
     );
   }
 }
