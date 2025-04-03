718267e9ac0c602083007ebd663ebb3908927c63
kindo-adama <80959098+k-adama@users.noreply.github.com>
2025-04-03 09:10:07 +0000

    fix image border

--- lib/core/widgets/components/app_login_register_header.dart
+++ lib/core/widgets/components/app_login_register_header.dart
@@ -10,37 +10,39 @@ class AppLoginRegisterHeader extends StatelessWidget {
     this.isNotHome = true,
     this.isLogin = true,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: isLogin ? 310 : 210,
       decoration: BoxDecoration(
-        image: DecorationImage(
-          image: isNotHome == true
-              ? const AssetImage('assets/images/header.png')
-              : const AssetImage('assets/images/home-header.png'),
-         fit: BoxFit.cover,
-        ),
-      ),
+          image: DecorationImage(
+            image: isNotHome == true
+                ? const AssetImage('assets/images/header.png')
+                : const AssetImage('assets/images/home-header.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(40),
+          )),
       child: isLogo == true
           ? Center(
               child: Image.asset('assets/icons/logo-voyage.png'),
             )
           : const Center(
-            child: Text(
-              "ENVIE D'UN SÉJOUR DE QUALITÉ SANS\n"
-              "SOUCIS DE DÉPLACEMENT ?\n"
-              "VOUS ÊTES À LA BONNE PORTE.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+              child: Text(
+                "ENVIE D'UN SÉJOUR DE QUALITÉ SANS\n"
+                "SOUCIS DE DÉPLACEMENT ?\n"
+                "VOUS ÊTES À LA BONNE PORTE.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
     );
   }
 }
